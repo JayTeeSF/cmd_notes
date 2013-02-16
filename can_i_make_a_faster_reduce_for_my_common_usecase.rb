d55a4c9ff4654fd437419a5dc8078ea7830d0eba
jayteesf <jonathan@his-service.net>
2013-02-16 08:44:45 -0800

    new benchmark, git config syntax, and comparison of github-repo branches

--- can_i_make_a_faster_reduce_for_my_common_usecase.rb
+++ can_i_make_a_faster_reduce_for_my_common_usecase.rb
@@ -1,6 +1,7 @@
 # require "#{ENV['HOME']}/dev/cmd_notes/can_i_make_a_faster_reduce_for_my_common_usecase"
 
 require 'benchmark'
+require 'set'
 Enumerable.class_eval do
   def fold(final)
     self.each{|i| final = yield(final, i) }
@@ -10,9 +11,15 @@ end
 
 module CompareFoldAndReduce
   extend self
+  SAMPLE_DATA = [1,2,3,4,5,6,7,8,9,10].freeze
+  SET_DATA = Set.new( SAMPLE_DATA ).freeze
+
+  def set_data
+    SET_DATA
+  end
 
   def sample_data
-    [1,2,3,4,5,6,7,8,9,10]
+    SAMPLE_DATA
   end
 
   def num_samples
@@ -35,8 +42,28 @@ module CompareFoldAndReduce
   #   lambda {|memo, i| memo + i}
   # end
 
+  def sample_set_fold
+    set_data.fold(0) {|memo, i| memo + i}
+  end
+
   def sample_fold
     sample_data.fold(0) {|memo, i| memo + i}
+  end
+
+  def sum
+    sample_data.sum
+  end
+
+  def better_set_reduce
+    set_data.reduce(:+) || 0
+  end
+
+  def better_reduce
+    sample_data.reduce(:+) || 0
+  end
+
+  def sample_set_reduce
+    set_data.reduce(0) {|memo, i| memo + i}
   end
 
   def sample_reduce
@@ -45,11 +72,23 @@ module CompareFoldAndReduce
 
   def results
     Benchmark.bmbm do |b|
-      b.report("sample_fold") do
-        num_samples.times { send("sample_fold") }
+      b.report("sample_set_reduce") do
+        num_samples.times { send("sample_set_reduce") }
+      end
+      b.report("better_set_reduce") do
+        num_samples.times { send("better_set_reduce") }
+      end
+      b.report("sample_set_fold") do
+        num_samples.times { send("sample_set_fold") }
       end
       b.report("sample_reduce") do
         num_samples.times { send("sample_reduce") }
+      end
+      b.report("sample_fold") do
+        num_samples.times { send("sample_fold") }
+      end
+      b.report("better_reduce") do
+        num_samples.times { send("better_reduce") }
       end
     end
   end
