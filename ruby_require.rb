903e504612600361e6d12c643432f4a1f54f3c97
jayteesf <jonathan@his-service.net>
2013-05-19 15:14:08 -0700

    corrected syntax for require

--- ruby_require.rb
+++ ruby_require.rb
@@ -2,6 +2,9 @@
 # relative to current file, find all ruby files
 # then strip the ".rb" extension before calling "require"
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each do |file| 
-    require File.basename(file, File.extname(file))
+    file_base = File.basename(file, File.extname(file))
+    file_dirname = File.dirname(file)
+    require_path = "#{file_dirname}/#{file_base}"
+    require require_path
 end
 
