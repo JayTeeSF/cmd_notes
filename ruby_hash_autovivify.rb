4d22ed5b44b017c7880fae6943bafa2cd9a9b1c3
jayteesf <jonathan@zoodles.com>
2014-02-05 19:21:08 -0800

    autovivify hash

--- ruby_hash_autovivify.rb
+++ ruby_hash_autovivify.rb
@@ -1,4 +1,4 @@
-owing to my days of Perl, I liek the following method:  
+# owing to my days of Perl, I like the following method:
 def autovivifying_hash(depth=0)
   return Hash.new if 0 == depth
   Hash.new { |ht,k| ht[k] =autovivifying_hash(depth - 1) }
@@ -7,3 +7,7 @@ end
 i.e.
 normal_h = autovivify_hash
 deeper_h = autovivify_hash(2)
+
+## or, better yet:
+
+Hash.new {|h,k| h[k] = Hash.new(&h.default_proc) }
