930cbc5a9875218f0052aae82f01d02b358767d3
JayTeeSF <jonathan@his-service.net>
2017-01-22 20:45:20 -0800

    updated cmds: shell-based web-browser/-crawler; process for 'fast' rails inserts/updates; trick for synchronizing a command with an already running command (i.e. detect when first command finishes)

--- scriptable_web_crawler.sh
+++ scriptable_web_crawler.sh
@@ -1,1 +1,25 @@
-printf "GET / HTTP/1.0\r\n\r\n" | nc $IP $PORT
+export NC_CMD=`which nc`
+#echo -n "GET / HTTP/1.0\r\n\r\n" | nc $IP $PORT
+read -p "Path [/]: " PATH
+case $PATH in
+  [/]* ) echo "";;
+  * ) export PATH="/";;
+esac
+
+read -p "HOSTNAME/IP [www.google.com]: " IP
+if [ "${IP}" = "" ]; then
+  export IP="www.google.com"
+fi
+
+case $PATH in
+  [/]* ) echo "";;
+  * ) export PATH="/";;
+esac
+
+
+read -p "PORT [80]: " PORT
+case $PORT in
+  [0123456789]* ) echo "";;
+  * ) export PORT=80;;
+esac
+printf "GET ${PATH} HTTP/1.0\r\n\r\n" | ${NC_CMD} ${IP} ${PORT}
