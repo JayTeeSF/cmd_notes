216c902fe115aee4ed40e7b47c89ac977a475ae8
JayTeeSF <jonathan@his-service.net>
2017-02-10 17:18:26 -0800

    no tape over webcam

---
+++ disable_webcam.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p $HOME/Documents/WebCamBackupFile/
+sudo mv /System/Library/QuickTime/QuickTimeUSBVDCDigitizer.component $HOME/Documents/WebCamBackupFile/
---
+++ enable_webcam.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo mv $HOME/Documents/WebCamBackupFile/QuickTimeUSBVDCDigitizer.component /System/Library/QuickTime/
