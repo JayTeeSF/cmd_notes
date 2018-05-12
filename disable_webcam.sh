8ec7b132b3788f1ed1ec27e5bec1598ddd730d44
JayTeeSF <jonathan@his-service.net>
2018-05-12 12:32:08 -0700

    Add mysql start/stop cmds

--- disable_webcam.sh
+++ disable_webcam.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# cmd + R at boottime
+sudo csrutil disable # <-- only works from recovery mode!!!
+
+# then, after reboot:
 mkdir -p $HOME/Documents/WebCamBackupFile/
 sudo mv /System/Library/QuickTime/QuickTimeUSBVDCDigitizer.component $HOME/Documents/WebCamBackupFile/
